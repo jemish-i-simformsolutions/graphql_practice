c5c223b7ac9d8688f7137067bfc090292696ddb3
Jemish Italiya <italiyajemish99@gmail.com>
2021-04-02 13:40:19 +0530

    Revert "Revert "commit while learning""
    This reverts commit fa611faaa57e5c788d9b34f80df693bb489315e4.

--- client/src/App.js
+++ client/src/App.js
@@ -7,33 +7,14 @@ import BookDetail from "./Component/BookDetail";
 import GetBook from "./Component/GetBook";
 import Hooks from "./Component/Hooks";
 import AddAuthor from "./Component/AddAuthor";
-import { WebSocketLink } from "@apollo/client/link/ws";
-import { getMainDefinition } from "@apollo/client/utilities";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4001/graphql",
 });
-const wsLink = new WebSocketLink({
-  uri: "ws://localhost:4001/graphql",
-  options: {
-    reconnect: true,
-  },
-});
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
-);
+
 const client = new ApolloClient({
   uri: "http://localhost:4001/graphql",
   cache: new InMemoryCache(),
-  splitLink,
 });
 
 function App() {
