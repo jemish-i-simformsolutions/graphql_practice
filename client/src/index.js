9da4fbbeac63863d886ec52e428370251e730ec9
Jemish Italiya <italiyajemish99@gmail.com>
2021-04-02 13:43:18 +0530

    Revert "Revert "commit while learning""
    This reverts commit ad73e2eee851a7bfee23f665968ce644229adc5a.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import About from "./Component/About";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
+    <About />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
